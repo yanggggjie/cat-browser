d1bb5eb8bc5ef603801edd9ec679c453798aea1a
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2023-08-19 14:42:48 +0800

    add qs

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,22 +1,9 @@
 import { clsx } from 'clsx'
 import _ from 'lodash-es'
-import axios from 'axios'
-import useSWR from 'swr'
-import { useState } from 'react'
+interface Props {}
 
-function Test() {
-  const { data, isLoading, error } = useSWR('/api')
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>Error</div>
-  return <div>{data}</div>
-}
-
-function Component() {
-  return (
-    <div>
-      <Test></Test>
-    </div>
-  )
+function Component({}: Props) {
+  return <div></div>
 }
 
 export default Component
