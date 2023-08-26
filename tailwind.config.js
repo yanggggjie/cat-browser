db0c10848067ba31bf66ae95a5dc4f017c2dce4f
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2023-08-26 19:31:57 +0800

    remove MUI, it is too heavy!

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,8 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   corePlugins: {
-    preflight: false,
+    preflight: true,
   },
-  important: '#root',
   theme: {
     extend: {},
   },
