d63d6656b3c7a81a10107da007ab3729f18417c9
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2023-08-29 09:55:26 +0800

    add globalAxios with interceptor

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,9 +1,18 @@
 import { clsx } from 'clsx'
 import _ from 'lodash-es'
+import globalAxios from '../globalAxios.ts'
 interface Props {}
 
 function Component({}: Props) {
-  return <div></div>
+  async function handleClick() {
+    const res = await globalAxios.get('/artists/4Z8W4fKeB5YxbusRsdQVPb')
+    console.log(res.data)
+  }
+  return (
+    <div>
+      <button onClick={handleClick}>click</button>
+    </div>
+  )
 }
 
 export default Component
