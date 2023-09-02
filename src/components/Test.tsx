2c06d5bd8aff029bed4424482e99ae290ea233f0
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2023-09-02 10:34:21 +0800

    remove @types/node, this is a completely CSR template!

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,28 +1,26 @@
-import { clsx } from 'clsx'
-import _ from 'lodash-es'
-interface Props {}
-import { useWindowScroll } from '@uidotdev/usehooks'
-import { useEffect, useState } from 'react'
-function Component() {
-  const [{ x, y }, scrollTo] = useWindowScroll()
+import { Suspense, useEffect, useState } from 'react'
 
-  const defaultData = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-  const [data, setData] = useState(defaultData)
+function Sub({ count }: { count: number }) {
+  return <div>count is {count}</div>
+}
+const App = () => {
+  const [count, setCount] = useState(0)
+
   useEffect(() => {
-    const pageBottom = document.documentElement.scrollHeight
-    const scrollBottom = y + window.innerHeight
-    if (pageBottom - scrollBottom < 100) {
-      setData([...data, ...defaultData])
-    }
-  }, [y])
+    setInterval(() => {
+      setCount((count) => {
+        return count + 1
+      })
+    }, 10000)
+  }, [])
 
   return (
-    <div className={clsx('space-y-40')}>
-      {data.map((value, index) => {
-        return <div key={index}>{value}</div>
-      })}
-    </div>
+    <>
+      <Suspense fallback={<div>loading...</div>}>
+        <Sub count={count} />
+      </Suspense>
+      <div>count is {count}</div>
+    </>
   )
 }
-
-export default Component
+export default App
--- temp/api.js
+++ temp/api.js
@@ -1,1 +1,1 @@
-const url = 'https://pokeapi.co/api/v2/ability/?limit=20&offset=20'
+const url = 'https://pokeapi.co/api/v2/ability/?limit=20&offset=0'
