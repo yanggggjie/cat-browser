2ffcd9aef54201c760fcf43f258135657b0c6d72
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2023-09-04 15:52:52 +0800

    refactor fetcher and axios

--- src/main.tsx
+++ src/main.tsx
@@ -3,34 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { SWRConfig } from 'swr'
-import globalAxios from './globalAxios.ts'
+import { fetcher } from './globalAxios.ts'
 import { BrowserRouter } from 'react-router-dom'
-
-export function sortQuery(url: string) {
-  const _url = new URL(url)
-  _url.searchParams.sort()
-  return _url.toString()
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   <BrowserRouter>
     <SWRConfig
       value={{
-        fetcher: async (url: string) => {
-          /*
-           ! useSWR use url as cache key
-           ! make sure different query string order has the same cache key
-           */
-          try {
-            const res = await globalAxios.get(sortQuery(url))
-            return res.data
-          } catch (e) {
-            console.log('error in fetcher')
-            console.log(`url: ${url}`)
-            console.log(e)
-          }
-        },
+        fetcher,
         revalidateOnFocus: false,
       }}
     >
