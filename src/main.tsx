5d119ce2ae980b9193395ef511c0f7dbe58fe9a2
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2023-09-04 20:36:21 +0800

    add global fetcher

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from './App.tsx'
 import './index.css'
 import { SWRConfig } from 'swr'
 import { BrowserRouter } from 'react-router-dom'
+import globalFetcher from './globalFetcher.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   <BrowserRouter>
     <SWRConfig
       value={{
+        fetcher: globalFetcher,
         revalidateOnFocus: false,
       }}
     >
